235f27002e9a353b3df4d2206c8e7836c1d32bfe
MohamedElsay0d <me827794@gmail.com>
2024-08-16 13:13:15 -0700

    create Note Card

--- lib/widgets/NotesViewBody.dart
+++ lib/widgets/NotesViewBody.dart
@@ -1,23 +1,37 @@
 import 'package:flutter/material.dart';
 
 import 'CustomAppBar.dart';
+import 'ViewCard.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 15.0),
+        padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 15.0),
         child: Column(
           children: [
-            SizedBox(
+            const SizedBox(
               height: 15,
             ),
-            CutomerAppBar(),
-            SizedBox(
+            const CutomerAppBar(),
+            const SizedBox(
               height: 10,
+            ),
+            Expanded(
+              child: ListView.separated(
+                itemCount: 20,
+                itemBuilder: (context, index) {
+                  return NoteCard();
+                },
+                separatorBuilder: (BuildContext context, int index) {
+                  return const SizedBox(
+                    height: 10,
+                  );
+                },
+              ),
             ),
           ],
         ),
---
+++ lib/widgets/ViewCard.dart
@@ -0,0 +1,71 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class NoteCard extends StatelessWidget {
+  NoteCard({super.key});
+
+  final Random _random = Random();
+
+  Color _getRandomColor() {
+    Color generateColor() {
+      return Color.fromARGB(
+        255,
+        _random.nextInt(256),
+        _random.nextInt(256),
+        _random.nextInt(256),
+      );
+    }
+
+    Color color;
+    do {
+      color = generateColor();
+    } while (_isColorBright(color));
+
+    return color;
+  }
+
+  bool _isColorBright(Color color) {
+    final double brightness =
+        (0.299 * color.red + 0.587 * color.green + 0.114 * color.blue);
+    return brightness > 186;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 140,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        color: _getRandomColor(),
+      ),
+      child: const Column(
+        children: [
+          ListTile(
+            title: Text(
+              "Title",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            subtitle: Text(
+              "Sub Title",
+              style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold),
+            ),
+            trailing: Icon(
+              Icons.delete,
+              size: 50,
+              color: Colors.red,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(right: 25.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [Text("data")],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
