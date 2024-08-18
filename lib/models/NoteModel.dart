53daeae3e0f211eeaaca21677b9ae1b42a812ea5
MohamedElsay0d <me827794@gmail.com>
2024-08-18 03:48:40 -0700

    make NotemodelAdapter

--- lib/models/NoteModel.dart
+++ lib/models/NoteModel.dart
@@ -1,6 +1,12 @@
-class Notemodel {
+import 'package:hive/hive.dart';
+
+@HiveType(typeId: 0)
+class Notemodel extends HiveObject {
+  @HiveField(0)
   String title;
+  @HiveField(1)
   String content;
+  @HiveField(2)
   String date;
   Notemodel({required this.title, required this.content, required this.date});
 }
---
+++ lib/models/NoteModel.g.dart
@@ -0,0 +1,22 @@
+import 'package:hive/hive.dart';
+import 'NoteModel.dart';
+
+class NotemodelAdapter extends TypeAdapter<Notemodel> {
+  @override
+  final int typeId = 0;
+
+  @override
+  Notemodel read(BinaryReader reader) {
+    final title = reader.readString();
+    final content = reader.readString();
+    final date = reader.readString();
+    return Notemodel(title: title, content: content, date: date);
+  }
+
+  @override
+  void write(BinaryWriter writer, Notemodel obj) {
+    writer.writeString(obj.title);
+    writer.writeString(obj.content);
+    writer.writeString(obj.date);
+  }
+}
